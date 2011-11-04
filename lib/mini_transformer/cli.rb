ab8a41adbb022f3f8cddbdcbb42c581d05bf3466
John Wang <john@johntwang.com>
2011-11-03 22:43:48 -1000

    CLI class for Command Line calls
    Add a CLI class to handle command line calls to the transformer

---
+++ lib/mini_transformer/cli.rb
@@ -0,0 +1,33 @@
+module MiniTransformer
+  class CLI < Thor
+    include Thor::Actions
+    
+    desc "transform [input.json, input.xml, output.html, format]", "Transform the files into format"
+    def transform(json, xml, output=nil, format="html", mapping=nil)
+      if output.nil?
+        output = "#{Dir.pwd}/test.html"
+      end
+
+      @parser = MiniTransformer::Parser.new
+      @parser.setup(json, xml, output, format)
+      @parser.validate
+      @parser.parse
+      
+      if format == "json"
+        puts @parser.to_json
+      else
+        @parser.to_html
+      end
+      
+    end
+    map %w(-t --tranform) => :tranform
+    
+    desc "version", "Prints the bundler's version information"
+    def version
+      puts "Mini Transform version #{MiniTransformer::VERSION}"
+      "Mini Transform version #{MiniTransformer::VERSION}"
+      #Bundler.ui.info "Bundler version #{Bundler::VERSION}"
+    end
+    map %w(-v --version) => :version
+  end
+end
